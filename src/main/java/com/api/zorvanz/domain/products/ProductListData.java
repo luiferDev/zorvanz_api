cc6b643b9a0acd80a5050881838ed715d27edaa3
Jorge Morales <luifer991@gmail.com>
2024-08-14 17:05:22 -0500

    get category data for ProductListData

--- src/main/java/com/api/zorvanz/domain/products/ProductListData.java
+++ src/main/java/com/api/zorvanz/domain/products/ProductListData.java
@@ -1,20 +1,24 @@
 package com.api.zorvanz.domain.products;
 
+import com.api.zorvanz.domain.category.CategoryData;
+
 public record ProductListData(
+        Long id,
         String name,
         String description,
         Double price,
-        Long categoryId,
+        CategoryData category,
         Integer stock,
         Double popularity,
         String imageUrl
 ) {
     public ProductListData(Product product) {
         this(
+                product.getId(),
                 product.getName(),
                 product.getDescription(),
                 product.getPrice(),
-                product.getCategory().getId(),
+                new CategoryData( product.getCategory() ),
                 product.getStock(),
                 product.getPopularity(),
                 product.getImageUrl()
