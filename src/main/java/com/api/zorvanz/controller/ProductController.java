60a74717e7c31e7b76c6d62d7f9bbceda4a04b0b
Jorge Morales <luifer991@gmail.com>
2024-08-15 23:42:08 -0500

    get request by id and post products controllers

--- src/main/java/com/api/zorvanz/controller/ProductController.java
+++ src/main/java/com/api/zorvanz/controller/ProductController.java
@@ -1,15 +1,15 @@
 package com.api.zorvanz.controller;
 
-import com.api.zorvanz.domain.products.ProductListData;
-import com.api.zorvanz.domain.products.ProductRepository;
+import com.api.zorvanz.domain.category.CategoryData;
+import com.api.zorvanz.domain.products.*;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
@@ -17,10 +17,26 @@ public class ProductController {
     
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private RegisterProductService registerProductService;
     
     @GetMapping
     public ResponseEntity<Page<ProductListData>> getProducts(
             @PageableDefault( page = 0, size = 8, sort = "popularity" )Pageable pagination ) {
         return ResponseEntity.ok( productRepository.findAll( pagination ).map( ProductListData::new ) );
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductListData> getProductById( @PathVariable Long id ) {
+        Product product = productRepository.getReferenceById( id );
+        return ResponseEntity.ok( new ProductListData( product ) );
+    }
+    
+    @PostMapping
+    @Transactional
+    public ResponseEntity createProduct(
+            @RequestBody @Valid RegisterProductData registerProduct) {
+        var response = registerProductService.registerProduct( registerProduct );
+        return ResponseEntity.ok( response );
     }
 }
