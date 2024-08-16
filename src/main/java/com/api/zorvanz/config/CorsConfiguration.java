7a65b0c37bcf2a72932fc1dbe156f5df08f09df3
Jorge Morales <luifer991@gmail.com>
2024-08-15 23:39:34 -0500

    production cors configuration

--- src/main/java/com/api/zorvanz/config/CorsConfiguration.java
+++ src/main/java/com/api/zorvanz/config/CorsConfiguration.java
@@ -9,8 +9,11 @@ public class CorsConfiguration implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings( CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+        registry.addMapping( "/**" )
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://zorvanz.vercel.app/"
+                )
+                .allowedMethods( "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT" );
     }
 }
