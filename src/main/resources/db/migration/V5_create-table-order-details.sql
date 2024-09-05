dc2efe346bd3e0658ed66296cebc5316800a7d40
Jorge Morales <luifer991@gmail.com>
2024-09-04 22:58:44 -0500

    add total amount to order detail table

--- src/main/resources/db/migration/V5_create-table-order-details.sql
+++ src/main/resources/db/migration/V5_create-table-order-details.sql
@@ -1,9 +1,10 @@
-create table products (
+create table order_details (
       id bigint not null auto_increment,
       order_id bigint not null,
       product_id bigint not null,
       quantity int not null,
       unit_price decimal(10,2) not null,
+      total_amount decimal(10,2) not null,
 
       primary key (id),
       foreign key (order_id) references orders(id),
