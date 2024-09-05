d99610fabd03f35d374a83c79349495785d3e52c
Jorge Morales <luifer991@gmail.com>
2024-09-04 23:01:35 -0500

    invoices created

---
+++ src/main/java/com/api/zorvanz/invoices/Invoices.java
@@ -0,0 +1,12 @@
+package com.api.zorvanz.invoices;
+
+import com.api.zorvanz.domain.orders.Orders;
+
+//@Entity(name = "Invoices")
+//@Table(name = "invoices")
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@EqualsAndHashCode (of = "id")
+public class Invoices {
+    public Orders orderId;
+}
