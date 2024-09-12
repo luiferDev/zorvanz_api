76d8b71eb27f52f9fa6b2c92edc0d1e7419d2b3b
Jorge Morales <luifer991@gmail.com>
2024-09-12 16:12:09 -0500

    change total amount type to big decimal

--- src/main/java/com/api/zorvanz/domain/cart/CartData.java
+++ src/main/java/com/api/zorvanz/domain/cart/CartData.java
@@ -2,11 +2,12 @@ package com.api.zorvanz.domain.cart;
 
 import com.api.zorvanz.domain.cartitem.CartItemData;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public record CartData(
         Long id,
-        Double totalAmount,
+        BigDecimal totalAmount,
         List <CartItemData> cartItems
 ) {
 }
