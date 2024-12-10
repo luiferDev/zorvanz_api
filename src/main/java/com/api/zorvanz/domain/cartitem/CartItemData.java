fef2fb901a1b2de6f63f8b84269ed8275025b5ab
luifer991 <luifer991@gmail.com>
2024-12-09 23:36:38 -0500

    adding information and constructor

--- src/main/java/com/api/zorvanz/domain/cartitem/CartItemData.java
+++ src/main/java/com/api/zorvanz/domain/cartitem/CartItemData.java
@@ -1,10 +1,25 @@
 package com.api.zorvanz.domain.cartitem;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public record CartItemData(
-        Long id,
-        Long cartId,
-        Long productId,
-        Integer quantity
+		Long id,
+		Long customerId,
+		Long productId,
+		Integer quantity,
+		BigDecimal unitPrice,
+		BigDecimal totalPrice
 ) {
+	public CartItemData(CartItem cartItem) {
+		this(
+				cartItem.getId(),
+				cartItem.getCustomer().getId(),
+				cartItem.getProduct().getId(),
+				cartItem.getQuantity(),
+				cartItem.getUnitPrice(),
+				cartItem.getTotalPrice()
+		);
+	}
 }
 
