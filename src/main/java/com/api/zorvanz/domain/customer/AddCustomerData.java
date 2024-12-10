8f809a88e623cf8cef725de9e46a014bd319b531
luifer991 <luifer991@gmail.com>
2024-12-09 23:28:59 -0500

    delete id from adding customer

--- src/main/java/com/api/zorvanz/domain/customer/AddCustomerData.java
+++ src/main/java/com/api/zorvanz/domain/customer/AddCustomerData.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record AddCustomerData(
-        Long id,
         @NotBlank
         String name,
         @NotBlank
