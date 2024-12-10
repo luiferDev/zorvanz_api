54b6b0bd16e07e12fc3c50dfa3746f26225e6ce6
luifer991 <luifer991@gmail.com>
2024-12-09 23:56:57 -0500

    removing unused imports

--- src/main/java/com/api/zorvanz/domain/products/ProductRepository.java
+++ src/main/java/com/api/zorvanz/domain/products/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.api.zorvanz.domain.products;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
     boolean existsByName ( String name );
