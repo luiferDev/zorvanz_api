009cfbe534ed8f90e0be3b11abf662d3f0ea853f
luifer991 <luifer991@gmail.com>
2024-12-09 23:34:58 -0500

    improving dependency injection, adding get methods to interact with the customer's cart and create a cart

--- src/main/java/com/api/zorvanz/controller/CartController.java
+++ src/main/java/com/api/zorvanz/controller/CartController.java
@@ -1,48 +1,76 @@
 package com.api.zorvanz.controller;
 
 import com.api.zorvanz.domain.cart.*;
-import com.api.zorvanz.domain.products.ProductRepository;
+import com.api.zorvanz.domain.cartitem.CartItem;
+import com.api.zorvanz.domain.cartitem.CartItemData;
+import com.api.zorvanz.domain.cartitem.CartItemRepository;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping ( "/api/cart" )
 public class CartController {
-    
-    @Autowired
-    private CartRepository cartRepository;
-    
-    @Autowired
-    private CartService cartService;
-    
-    @Autowired
-    private ProductRepository productRepository;
-    
-    @PostMapping
-    public ResponseEntity <?> createCart (
-            @RequestBody @Valid CartRegisterData cartRegisterData, UriComponentsBuilder uriBuilder
-    ) {
-        
-        var cart = cartService.createCart( cartRegisterData );
-        
-        var uri = uriBuilder.path( "/api/cart/{id}" )
-                .buildAndExpand( cart.id() ).toUri();
-        return ResponseEntity.created( uri ).body( cart );
-    }
-    
-    @GetMapping ("/{id}")
-    public ResponseEntity<CartData> getCartById( @PathVariable Long id, CartRegisterData data ) {
-        Cart cart = cartRepository.getReferenceById( id );
-//        Cart cart1 = new Cart();
-//        var products = productRepository.findAllById( cartService.getProductsbyId( data ) );
-//        var cartItems = cartService.getCartItem( data, products, cart1 );
-//        var totalAmount = cartService.getTotalAmount( cartItems );
-//        cart1.setCartItems( cartItems );
-//        cart1.setTotalAmount( totalAmount );
-        // TODO: get cartItemData
-        return ResponseEntity.ok( new CartData( cart.getId(), null, null ) );
-    }
+	private final CartItemRepository cartItemRepository;
+	private final CartService cartService;
+	
+	public CartController ( CartItemRepository cartItemRepository,
+	                        CartService cartService ) {
+		this.cartItemRepository = cartItemRepository;
+		this.cartService = cartService;
+	}
+	
+	// para crear un carrito debo entregarle una lista de cartItem
+	// estos tendrán que calcular
+	@PostMapping
+	@Transactional
+	public ResponseEntity<CartResponse> createCart(
+			@RequestBody @Valid CartRegisterData cartRegister,
+			UriComponentsBuilder uriBuilder
+	) {
+		var cart = cartService.createCart(cartRegister);
+		var uri = uriBuilder.path("/api/cart/{id}")
+				.buildAndExpand(cart.id()).toUri();
+		return ResponseEntity.created(uri).body(cart);
+	}
+
+	@GetMapping ( "/{id}" )
+	public ResponseEntity<CartResponse> getCartById ( @PathVariable Long id ) {
+		var response = cartService.getCartById(id);
+		if ( response == null ) {
+			return ResponseEntity.notFound ().build ();
+		}
+
+		return ResponseEntity.ok ( response );
+	}
+
+	@GetMapping ( "/get-carts" )
+	public ResponseEntity<List <CartResponse>> getCart () {
+		var response = cartService.getCart ();
+		if ( response == null ) {
+			return ResponseEntity.notFound ().build ();
+		}
+
+		return ResponseEntity.ok ( response );
+	}
+
+	@GetMapping ( "/customer-cart/{id}" )
+	public ResponseEntity<CartResponse> getCustomerCartById ( @PathVariable Long id ) {
+		var response = cartService.getCartByCustomerId(id);
+		if ( response == null ) {
+			return ResponseEntity.notFound ().build ();
+		}
+
+		return ResponseEntity.ok ( response );
+	}
+	//TODO: implementar el método para actualizar un carrito
+	//@PutMapping ( "/{id}" )
+	//TODO: implementar el método para agregar un item al carrito
+	//@PostMapping ( "/{id}" )
+	//TODO: implementar el método para eliminar un carrito
+	//@DeleteMapping ( "/{id}" )
 }
