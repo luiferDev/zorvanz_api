53c10ebf32bd6cb92d46464e25e76ce1a94e8a03
luifer991 <luifer991@gmail.com>
2024-12-09 23:52:15 -0500

    service layer for create cart and get methods

--- src/main/java/com/api/zorvanz/domain/cart/CartService.java
+++ src/main/java/com/api/zorvanz/domain/cart/CartService.java
@@ -1,102 +1,108 @@
 package com.api.zorvanz.domain.cart;
 
-import com.api.zorvanz.domain.cartitem.CartItem;
-import com.api.zorvanz.domain.cartitem.CartItemRegister;
-import com.api.zorvanz.domain.cartitem.CartItemRepository;
+import com.api.zorvanz.domain.cartitem.*;
+import com.api.zorvanz.domain.customer.Customer;
 import com.api.zorvanz.domain.products.Product;
 import com.api.zorvanz.domain.products.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.math.RoundingMode;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CartService implements ICartService {
-    
-    @Autowired
-    private CartRepository cartRepository;
-    
-    @Autowired
-    private CartItemRepository cartItemRepository;
-    
-    @Autowired
-    private ProductRepository productRepository;
-    
-    private CartService () {
-    }
-    
-    public static CartService createCartService () {
-        return new CartService();
-    }
-    
-    @Override
-    public CartResponse createCart ( CartRegisterData cartRegisterData ) {
-        
-        // Obtener los productos usando productIds
-        var products = productRepository.findAllById( getProductsbyId( cartRegisterData ) );
-        
-        // inicializar el Cart
-        Cart cart = new Cart( null, BigDecimal.ZERO, new ArrayList <>() );
-        
-        // Crear CartItems usando productos y cantidades
-        var cartItems = getCartItem( cartRegisterData, products, cart );
-        
-        var totalAmount = getTotalAmount( cartItems );
-        
-        // Asignar los CartItems al Cart
-        cart.setCartItems( cartItems );
-        cart.setTotalAmount( totalAmount );
-        
-        // Guardar CartItems y Cart
-        cartRepository.save( cart );
-        
-        return new CartResponse( cart );
-    }
-    
-    @Override
-    public BigDecimal getTotalAmount ( List <CartItem> cartItems ) {
-        return cartItems.stream()
-                .map( CartItem::getTotalPrice )
-                .reduce( BigDecimal.ZERO, BigDecimal::add );
-    }
-    
-    @Override
-    public List <Long> getProductsbyId ( CartRegisterData data ) {
-        // Obtener los productIds y las cantidades
-        List <Long> productIds = data.cartItems().stream()
-                .map( CartItemRegister::productId )
-                .toList();
-        return productIds;
-    }
-    
-    @Override
-    public List <CartItem> getCartItem ( CartRegisterData data, List<Product> products, Cart cart ) {
-        // obtener los item de los productos
-        return new ArrayList <>( data.cartItems().stream()
-                .map( cartItemRegister -> {
-                    Product product = products.stream()
-                            .filter( p -> p.getId().equals( cartItemRegister.productId() ) )
-                            .findFirst()
-                            .orElseThrow( () -> new IllegalArgumentException( "Product not found" ) );
-                    
-                    // Convertir quantity a BigDecimal
-                    BigDecimal quantity = BigDecimal.valueOf( cartItemRegister.quantity() );
-                    
-                    // Multiplicar price (BigDecimal) por quantity (BigDecimal)
-                    BigDecimal totalPrice = product.getPrice().multiply( quantity );
-                    
-                    return new CartItem(
-                            null,
-                            cartItemRegister.quantity(),       // Cantidad original
-                            product.getPrice(),                // Precio unitario como BigDecimal
-                            totalPrice,                        // Total calculado como BigDecimal
-                            product,                           // El producto
-                            cart                               // Cart se asignará después
-                    );
-                } )
-                .toList() );
-    }
+
+	private final CartRepository cartRepository;
+	private final CartItemRepository cartItemRepository;
+	private final ProductRepository productRepository;
+
+	private CartService ( CartRepository cartRepository,
+	                      CartItemRepository cartItemRepository,
+						  ProductRepository productRepository){
+		this.cartRepository = cartRepository;
+		this.cartItemRepository = cartItemRepository;
+		this.productRepository = productRepository;
+	}
+
+	@Override
+	public CartResponse createCart(CartRegisterData cartData) {
+		// get customer id
+		Long customerId = cartData.customerId();
+		if (customerId == null) {
+			throw new IllegalArgumentException("Customer ID cannot be null");
+		}
+		// get list cart Items
+		List<CartItemRegister> itemRegisters = cartData.cartItems();
+		if (itemRegisters == null || itemRegisters.isEmpty()) {
+			throw new IllegalArgumentException("Cart must contain at least one item");
+		}
+		// get customer
+		Customer customer = cartItemRepository.findByCustomerId(customerId)
+				.orElseThrow(() -> new IllegalArgumentException("Customer not found"));
+		// extract cart item for the products quantity unit price total price
+		List<CartItem> cartItems = itemRegisters.stream().map(item -> {
+			// Obtener el producto
+			Product product = productRepository.findById(item.productId())
+					.orElseThrow(() -> new IllegalArgumentException("Product not found"));
+
+			// Validar cantidad
+			int quantity = item.quantity();
+			if (quantity <= 0) {
+				throw new IllegalArgumentException("Invalid quantity");
+			}
+
+			// Calcular precio total
+			BigDecimal productUnitPrice = product.getPrice();
+			BigDecimal totalPrice = productUnitPrice.multiply(BigDecimal.valueOf(quantity))
+					.setScale(2, RoundingMode.HALF_UP);
+
+			return new CartItem(null, quantity, product, customer, productUnitPrice, totalPrice);
+		}).toList();
+
+		// total amount
+		BigDecimal totalAmount = calculateTotalAmount(cartItems);
+
+		// create cart
+		Cart cart = new Cart(null, totalAmount, customer, cartItems );
+		cartRepository.save(cart);
+
+		// Asociar el carrito con los items y guardar los items
+		cartItems.forEach(item -> item.setCart(cart));
+		cartItemRepository.saveAll(cartItems);
+
+		return new CartResponse(cart);
+	}
+
+
+	@Override
+	public BigDecimal calculateTotalAmount ( List <CartItem> cartItems ) {
+		return cartItems.stream ()
+				.map ( CartItem::getTotalPrice )
+				.filter ( Objects::nonNull )
+				.reduce ( BigDecimal.ZERO, BigDecimal::add )
+				.setScale ( 2, RoundingMode.HALF_UP );
+	}
+
+	@Override
+	public CartResponse getCartById ( Long id ) {
+		return cartRepository.findById ( id )
+				.map ( CartResponse::new )
+				.orElseThrow ( () -> new IllegalArgumentException ( "Cart not found" ) );
+	}
+
+	@Override
+	public List<CartResponse> getCart () {
+		return cartRepository.findAll ().stream ().map ( CartResponse::new ).toList ();
+	}
+
+	@Override
+	public CartResponse getCartByCustomerId( Long customerId) {
+		return cartRepository.findByCustomerId(customerId)
+				.map(CartResponse::new)
+				.orElseThrow(() -> new IllegalArgumentException("Cart not found"));
+	}
+
 }
 
