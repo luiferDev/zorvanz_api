f9be9abe16f2acc106676d5f8aaa2cd2e0cc3504
luifer991 <luifer991@gmail.com>
2024-12-09 23:32:17 -0500

    adding customer relation in order to create method can help to get customer's cart

--- src/main/java/com/api/zorvanz/domain/cart/Cart.java
+++ src/main/java/com/api/zorvanz/domain/cart/Cart.java
@@ -1,11 +1,13 @@
 package com.api.zorvanz.domain.cart;
 
 import com.api.zorvanz.domain.cartitem.CartItem;
+import com.api.zorvanz.domain.customer.Customer;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity ( name = "Carts" )
@@ -16,40 +18,17 @@ import java.util.List;
 @AllArgsConstructor
 @EqualsAndHashCode ( of = "id" )
 public class Cart {
-    @Id
-    @GeneratedValue ( strategy = GenerationType.IDENTITY )
-    private Long id;
-    private BigDecimal totalAmount = BigDecimal.ZERO;
-    
-    @OneToMany ( mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER )
-    private List <CartItem> cartItems = new ArrayList <>();
+	@Id
+	@GeneratedValue ( strategy = GenerationType.IDENTITY )
+	private Long id;
+	private BigDecimal totalAmount = BigDecimal.ZERO;
 
-//        public void addItem( CartItem cartItem, int quantity) {
-//            for ( CartItem item : cartItems ) {
-//                if ( item.getProduct().getId().equals( cartItem.getId() ) ) {
-//                    item.setQuantity( item.getQuantity() );
-//                    return;
-//                }
-//            }
-//
-//            var productEnt = new Product( );
-//            var unitPrice = productEnt.getPrice();
-//            var totalPrice = unitPrice * quantity;
-//
-//            cartItems.add( new CartItem( null, quantity, totalPrice, unitPrice, productEnt, this) );
-//        }
-//
-//        public void removeItem(Long productId) {
-//            cartItems.removeIf(item -> item.getProduct().getId().equals(productId));
-//        }
-    
-    public void setCartItems ( List <CartItem> cartItems ) {
-        this.cartItems = cartItems;
-        cartItems.forEach( item -> item.setCart( this ) );  // Asegura que cada CartItem tenga la referencia al Cart
-    }
-    
-    public void clearCart () {
-        cartItems.clear();
-    }
+	@ManyToOne ( fetch = FetchType.LAZY )
+	@JoinColumn ( name = "customer_id" )
+	private Customer customer;
+
+	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<CartItem> cartItems = new ArrayList <> ();
+
 }
 
