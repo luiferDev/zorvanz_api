a11984dee87df0fa7564d36dfc96e40706f9c6b5
luifer991 <luifer991@gmail.com>
2024-12-09 23:40:08 -0500

    find by customer id

--- src/main/java/com/api/zorvanz/domain/cart/CartRepository.java
+++ src/main/java/com/api/zorvanz/domain/cart/CartRepository.java
@@ -2,5 +2,8 @@ package com.api.zorvanz.domain.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long>{
+    Optional<Cart> findByCustomerId ( Long id );
 }
