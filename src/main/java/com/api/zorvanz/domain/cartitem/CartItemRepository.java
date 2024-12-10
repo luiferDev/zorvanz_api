707fee5f48f6781fa48d427d86e1ca8d87fea9fa
luifer991 <luifer991@gmail.com>
2024-12-09 23:39:02 -0500

    adding methods

--- src/main/java/com/api/zorvanz/domain/cartitem/CartItemRepository.java
+++ src/main/java/com/api/zorvanz/domain/cartitem/CartItemRepository.java
@@ -1,14 +1,23 @@
 package com.api.zorvanz.domain.cartitem;
 
 import com.api.zorvanz.domain.cart.Cart;
+import com.api.zorvanz.domain.customer.Customer;
 import com.api.zorvanz.domain.products.Product;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface CartItemRepository extends JpaRepository<CartItem, Long>{
-    int findByQuantity ( int quantity );
-    
-    List <Product> findByProductId ( Long productId );
-    Cart findByCartId ( Long cartId );
+public interface CartItemRepository extends JpaRepository <CartItem, Long> {
+	int findByQuantity ( int quantity );
+	
+	Product findByProductId ( Long productId );
+	
+	@Query ( "SELECT c FROM Customer c WHERE c.id = :customerId" )
+	Optional <Customer> findByCustomerId ( @Param ( "customerId" ) @NotNull Long customerId );
+
+	List <CartItem> findAllByCustomerId ( Long customerId );
 }
