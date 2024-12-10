3c4a173a0a69e63066acae0511bb45a251c07739
luifer991 <luifer991@gmail.com>
2024-12-09 23:37:20 -0500

    accept productid and quantity to create a cart

--- src/main/java/com/api/zorvanz/domain/cartitem/CartItemRegister.java
+++ src/main/java/com/api/zorvanz/domain/cartitem/CartItemRegister.java
@@ -3,9 +3,9 @@ package com.api.zorvanz.domain.cartitem;
 import jakarta.validation.constraints.NotNull;
 
 public record CartItemRegister(
-        @NotNull(message = "Campo \"productId\" es requerido")
-        Long productId,
-        @NotNull(message = "Campo \"quantity\" es requerido")
-        Integer quantity
+		@NotNull ( message = "Campo \"productId\" es requerido" )
+		Long productId,
+		@NotNull ( message = "Campo \"quantity\" es requerido" )
+		Integer quantity
 ) {
 }
