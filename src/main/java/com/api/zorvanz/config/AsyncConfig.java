caef2fe2a4b3dd4c01f23854cc324a5a8840e0d7
luifer991 <luifer991@gmail.com>
2024-12-12 19:58:03 -0500

    configuring async

---
+++ src/main/java/com/api/zorvanz/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.api.zorvanz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean ( name = "threadPoolTaskExecutor" )
+    public TaskExecutor getAsyncExecutor () {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor ();
+        executor.setCorePoolSize ( 4 );
+        executor.setMaxPoolSize ( 8 );
+        executor.setQueueCapacity ( 100 );
+        executor.setWaitForTasksToCompleteOnShutdown ( true );
+        executor.setThreadNamePrefix ( "AsyncThread-" );
+        executor.initialize ();
+        return executor;
+    }
+}
