ba85ac16494515679716361f0710c3b9b5299551
luifer991 <luifer991@gmail.com>
2024-12-12 19:59:17 -0500

    create query for getting total amount from cart

--- src/main/java/com/api/zorvanz/domain/cart/CartRepository.java
+++ src/main/java/com/api/zorvanz/domain/cart/CartRepository.java
@@ -1,9 +1,15 @@
 package com.api.zorvanz.domain.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long>{
     Optional<Cart> findByCustomerId ( Long id );
+	BigDecimal findTotalAmountByCustomerId ( Long customerId );
+	@Query("SELECT c.totalAmount FROM Carts c WHERE c.id = :cartId")
+	BigDecimal findTotalAmountById ( @Param ("cartId") Long cartId );
 }
