16a7a7181653b7a46593005dc52c623fefa81b5e
luifer991 <luifer991@gmail.com>
2024-12-12 20:04:42 -0500

    delete unnecessary queries

--- src/main/java/com/api/zorvanz/domain/cart/CartRepository.java
+++ src/main/java/com/api/zorvanz/domain/cart/CartRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long>{
     Optional<Cart> findByCustomerId ( Long id );
-	BigDecimal findTotalAmountByCustomerId ( Long customerId );
 	@Query("SELECT c.totalAmount FROM Carts c WHERE c.id = :cartId")
 	BigDecimal findTotalAmountById ( @Param ("cartId") Long cartId );
 }
