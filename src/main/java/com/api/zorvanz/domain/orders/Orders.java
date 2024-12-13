c0530276598993669a88804990af360237dcb0b4
luifer991 <luifer991@gmail.com>
2024-12-12 20:09:13 -0500

    change total amount to BigDecimal type and adding Cart and payment method

--- src/main/java/com/api/zorvanz/domain/orders/Orders.java
+++ src/main/java/com/api/zorvanz/domain/orders/Orders.java
@@ -1,9 +1,11 @@
 package com.api.zorvanz.domain.orders;
 
+import com.api.zorvanz.domain.cart.Cart;
 import com.api.zorvanz.domain.customer.Customer;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity ( name = "Orders" )
@@ -20,8 +22,14 @@ public class Orders {
     @ManyToOne (fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
     private Customer customer;
-    private Double totalAmount;
+    private BigDecimal totalAmount;
     @Enumerated(EnumType.STRING)
     private Status status;
     private LocalDateTime date;
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+    @Enumerated( EnumType.STRING )
+    private Payment paymentMethod;
+
 }
