b8b8b313a950eb8aae6b772a926a0ac53e32e923
luifer991 <luifer991@gmail.com>
2025-01-22 14:32:20 -0500

    add cart id and customer id validation

--- src/main/java/com/api/zorvanz/domain/cartitem/CartItemRegister.java
+++ src/main/java/com/api/zorvanz/domain/cartitem/CartItemRegister.java
@@ -6,6 +6,10 @@ public record CartItemRegister(
 		@NotNull ( message = "Campo \"productId\" es requerido" )
 		Long productId,
 		@NotNull ( message = "Campo \"quantity\" es requerido" )
-		Integer quantity
+		Integer quantity,
+		@NotNull ( message = "Campo \"customer id\" es requerido" )
+		Long customerId,
+		@NotNull ( message = "Campo \"cart id\" es requerido" )
+		Long cartId
 ) {
 }
