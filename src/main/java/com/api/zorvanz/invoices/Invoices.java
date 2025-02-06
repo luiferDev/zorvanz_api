8097ee8ffe6fba960a7191d17c629f22742a042d
luifer991 <luifer991@gmail.com>
2025-02-05 22:42:37 -0500

    invoices

--- src/main/java/com/api/zorvanz/invoices/Invoices.java
+++ src/main/java/com/api/zorvanz/invoices/Invoices.java
@@ -1,12 +1,26 @@
 package com.api.zorvanz.invoices;
 
 import com.api.zorvanz.domain.orders.Orders;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-//@Entity(name = "Invoices")
-//@Table(name = "invoices")
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@EqualsAndHashCode (of = "id")
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity (name = "Invoices")
+@Table (name = "invoices")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode (of = "id")
 public class Invoices {
-    public Orders orderId;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Orders orderId;
+    private Long customerId;
+    private String paymentMethod;
+    private LocalDateTime date;
+    private BigDecimal amount;
 }
