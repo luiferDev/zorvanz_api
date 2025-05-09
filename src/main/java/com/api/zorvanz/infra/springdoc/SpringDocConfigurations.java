4e597487eb05388e1c7eaef336ad7360974591e9
luifer991 <luifer991@gmail.com>
2025-05-08 23:42:46 -0500

    change spring doc

--- src/main/java/com/api/zorvanz/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/com/api/zorvanz/infra/springdoc/SpringDocConfigurations.java
@@ -5,32 +5,35 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfigurations {
-		@Bean
-		public OpenAPI customOpenAPI() {
-			return new OpenAPI()
-					.components(new Components ()
-							.addSecuritySchemes("bearer-key",
-									new SecurityScheme ()
-											.type(SecurityScheme.Type.HTTP)
-											.scheme("bearer")
-											.bearerFormat("JWT")))
-					.info(new Info ()
-							.title("API Voll.med")
-							.description("API Rest del ecommerce Zorvanz")
-							.contact(new Contact ()
-									.name("Equipo Backend")
-									.email("backend@voll.med"))
-							.license(new License()
-									.name("Apache 2.0")
-									.url("http://zorvanz/api/licencia")));  }
+    @Bean
+    public OpenAPI customOpenAPI () {
+        return new OpenAPI ()
+                .components ( new Components ()
+                        .addSecuritySchemes ( "bearer-key",
+                                new SecurityScheme ()
+                                        .type ( SecurityScheme.Type.HTTP )
+                                        .scheme ( "bearer" )
+                                        .bearerFormat ( "JWT" ) ) )
+                .addSecurityItem ( new SecurityRequirement ().addList ( "bearer-key" ) ) // 👈 Esto aplica el esquema
+                .info ( new Info ()
+                        .title ( "API Zorvanz" )
+                        .description ( "API Rest del ecommerce Zorvanz" )
+                        .contact ( new Contact ()
+                                .name ( "Equipo Backend" )
+                                .email ( "backend@zorvanz.co" ) )
+                        .license ( new License ()
+                                .name ( "Apache 2.0" )
+                                .url ( "http://zorvanz/api/licencia" ) ) );
+    }
 
-		public void message () {
-			System.out.println("bearer is working");
-		}
+    public void message () {
+        System.out.println ( "bearer is working" );
+    }
 }
