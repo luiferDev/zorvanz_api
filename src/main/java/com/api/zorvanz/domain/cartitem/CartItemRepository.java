66811c79680b5e27fff753566c61eb802e331664
luifer991 <luifer991@gmail.com>
2025-05-08 23:32:05 -0500

    Create finByCustomerId

--- src/main/java/com/api/zorvanz/domain/cartitem/CartItemRepository.java
+++ src/main/java/com/api/zorvanz/domain/cartitem/CartItemRepository.java
@@ -17,9 +17,9 @@ public interface CartItemRepository extends JpaRepository <CartItem, Long> {
 	Product findByProductId ( Long productId );
 	
 	@Query ( "SELECT c FROM Customer c WHERE c.id = :customerId" )
-	Optional <Customer> findByCustomerId ( @Param ( "customerId" ) @NotNull Long customerId );
+	Optional < Customer > findByCustomerId ( @Param ( "customerId" ) @NotNull Long customerId );
 
-	List <CartItem> findAllByCustomerId ( Long customerId );
+	List < CartItem > findAllByCustomerId ( Long customerId );
 
 	boolean existsByCustomerIdAndProductId ( Long id, Long id1 );
 
