f8b90c12af478e777f9a39de4d26d5f8f18fedda
luifer991 <luifer991@gmail.com>
2025-05-08 23:38:22 -0500

    creating constructor for the response

--- src/main/java/com/api/zorvanz/domain/products/ProductResponse.java
+++ src/main/java/com/api/zorvanz/domain/products/ProductResponse.java
@@ -1,5 +1,6 @@
 package com.api.zorvanz.domain.products;
 
+import com.api.zorvanz.domain.category.Categories;
 import com.api.zorvanz.domain.category.CategoryData;
 
 import java.math.BigDecimal;
@@ -12,4 +13,9 @@ public record ProductResponse(
         BigDecimal price,
         String imageUrl
 ) {
+    public ProductResponse ( Long id, String name,
+                             String description, Categories category,
+                             BigDecimal price, String imageUrl ) {
+        this ( id, name, description, new CategoryData ( category.getId (), category.getCategoryName () ), price, imageUrl );
+    }
 }
