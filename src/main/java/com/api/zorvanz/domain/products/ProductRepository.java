520d2f4b63469cb804118fff11f403b7ce05a1a0
luifer991 <luifer991@gmail.com>
2025-05-08 23:37:48 -0500

    adding method for searching by name of the product

--- src/main/java/com/api/zorvanz/domain/products/ProductRepository.java
+++ src/main/java/com/api/zorvanz/domain/products/ProductRepository.java
@@ -1,15 +1,15 @@
 package com.api.zorvanz.domain.products;
 
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository < Product, Long > {
     boolean existsByName ( String name );
-    
+
     @Query ( "SELECT p.price FROM Product p WHERE p.id IN :productIds" )
-    List <Double> findPricesByProductIds ( List <Long> productIds );
+    List < Double > findPricesByProductIds ( List < Long > productIds );
+
+    List < Product > findByNameStartingWithIgnoreCase ( String name );
 }
