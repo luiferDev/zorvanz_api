3e6d172a1e3f3e9c09624f69b34eb33583ee3677
luifer991 <luifer991@gmail.com>
2025-05-09 23:19:21 -0500

    add user Role to the response in order to get the role od the user

--- src/main/java/com/api/zorvanz/infra/security/AuthResponse.java
+++ src/main/java/com/api/zorvanz/infra/security/AuthResponse.java
@@ -1,7 +1,10 @@
 package com.api.zorvanz.infra.security;
 
+import com.api.zorvanz.domain.users.Role;
+
 public record AuthResponse(
         String accessToken,
         String refreshToken,
-        String tokenType
+        String tokenType,
+        Role role
 ) {}
