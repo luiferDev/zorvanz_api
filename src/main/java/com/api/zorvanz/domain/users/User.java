d8a9e219505dcc9b47e860d533ce195685068b76
luifer991 <luifer991@gmail.com>
2025-05-09 23:22:42 -0500

    create user and admin constructor

--- src/main/java/com/api/zorvanz/domain/users/User.java
+++ src/main/java/com/api/zorvanz/domain/users/User.java
@@ -30,6 +30,43 @@ public class User implements UserDetails {
     @Enumerated ( EnumType.STRING )
     private Role role;
 
+    /**
+     * Creates a new user with the USER role
+     */
+    public static User createUser ( String name, String lastName,
+                                    String password, String userName,
+                                    String email ) {
+        User user = new User ();
+        user.setName ( name );
+        user.setLastName ( lastName );
+        user.setPassword ( password );
+        user.setUserName ( userName );
+        user.setEmail ( email );
+        user.setRole ( Role.ROLE_USER );
+        return user;
+    }
+
+    /**
+     * Creates a new user with the ADMIN role
+     */
+    public static User createAdmin ( String name, String lastName,
+                                     String password, String userName, String email ) {
+        User user = new User ();
+        user.setName ( name );
+        user.setLastName ( lastName );
+        user.setPassword ( password );
+        user.setUserName ( userName );
+        user.setEmail ( email );
+        user.setRole ( Role.ROLE_ADMIN );
+        return user;
+    }
+
+    /**
+     * Returns the authorities granted to the user based on their role.
+     * This method is used by Spring Security to determine user permissions.
+     *
+     * @return a collection of granted authorities
+     */
     @Override
     public Collection < ? extends GrantedAuthority > getAuthorities () {
         return List.of ( new SimpleGrantedAuthority ( "ROLE_" + role ) );
